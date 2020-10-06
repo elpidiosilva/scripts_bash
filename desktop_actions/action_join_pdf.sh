a4fec2ac2d46b01ef1edba0a6e396f549381813e
user <72104486+elpidiosilva@users.noreply.github.com>
2020-10-06 17:15:43 -0300

    Primeiro commit no branch PDFs, com os scripts action_join_pdf.sh e action_split_pdf.sh

---
+++ desktop_actions/action_join_pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Tratamento do caminho e nome do arquivo
+fullpath=$1					# Caminho completo sem aspas, encaminhadas pelo Thunar
+filename="${fullpath##*/}"                      # Pega o nome do arquivo, considerando a última / encontrada no caminho
+dir="${fullpath:0:${#fullpath} - ${#filename}}" # Subtrai o nome do arquivo do caminho completo
+base="${filename%.[^.]*}"                       # Nome do arquivo sem extensão
+ext="${filename:${#base} + 1}"                  # Extensão do arquivo
+agora=$(date +"%s")
+new_filename="agrupado_${agora}.pdf"
+
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="${dir}${new_filename}" "$@" | zenity --progress --pulsate --auto-close --auto-kill --text "Agrupando PDFs ..."
+
+exit 0
---
+++ desktop_actions/action_split_pdf.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+while (( "$#" )); do
+	## Tratamento do caminho e nome do arquivo
+	fullpath=$1					# Caminho completo sem aspas, encaminhadas pelo Thunar
+	filename="${fullpath##*/}"                      # Pega o nome do arquivo, considerando a última / encontrada no caminho
+	dir="${fullpath:0:${#fullpath} - ${#filename}}" # Subtrai o nome do arquivo do caminho completo
+	base="${filename%.[^.]*}"                       # Nome do arquivo sem extensão
+	ext="${filename:${#base} + 1}"                  # Extensão do arquivo
+	agora=$(date +"%s")
+	new_folder="${base}_$agora"
+	mkdir "${dir}${new_folder}";pdfseparate "$1" "$new_folder"/pagina_%d.pdf| zenity --progress --pulsate --auto-close --auto-kill --text "Separando páginas de $1 ..."
+	shift
+done
+exit 0
